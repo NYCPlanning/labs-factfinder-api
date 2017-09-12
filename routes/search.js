ddeab69fe0b4aa0d343f6214aa11db5bdf1070bd
Chris Whong <chris@whong.org>
2017-09-12 17:41:35 -0400

    add zoning amendments

--- routes/search.js
+++ routes/search.js
@@ -9,7 +9,8 @@ function toTitleCase(str) {
 }
 
 function mapzenSearch(string) {
-  const mapzenSearchAPI = `https://search.mapzen.com/v1/autocomplete?focus.point.lat=40.7259&focus.point.lon=-73.9805&limit=5&api_key=${process.env.MAPZEN_API_KEY}&text=${string}`;
+  const mapzenSearchAPI =
+   `https://search.mapzen.com/v1/autocomplete?focus.point.lat=40.7259&focus.point.lon=-73.9805&limit=5&api_key=${process.env.MAPZEN_API_KEY}&boundary.rect.min_lon=-74.292297&boundary.rect.max_lon=-73.618011&boundary.rect.min_lat=40.477248&boundary.rect.max_lat=40.958123&text=${string}`;
 
   return rp(mapzenSearchAPI)
     .then(res => JSON.parse(res))
@@ -23,10 +24,11 @@ function mapzenSearch(string) {
         coordinates,
         type: 'address',
       };
-    }));
+    }))
+    .then(json => json.slice(1, 6));
 }
 
-function plutoSearch(text = '') {
+function plutoSearch(string) {
   const SQL = `
     SELECT (address || ', ' ||
       CASE
@@ -36,7 +38,8 @@ function plutoSearch(text = '') {
         WHEN borough = 'QN' THEN 'Queens'
         WHEN borough = 'SI' THEN 'Staten Island'
       END) as address, bbl FROM support_mappluto
-     WHERE address LIKE '%25${text.toUpperCase()}%25' LIMIT 5`;
+     WHERE address LIKE '%25${string.toUpperCase()}%25' LIMIT 5
+  `;
 
   return carto.SQL(SQL).then(rows =>
     rows.map((row) => {
@@ -47,23 +50,41 @@ function plutoSearch(text = '') {
     }));
 }
 
+function zmaSearch(string) {
+  const SQL = `
+    SELECT
+      project_na,
+      ulurpno
+    FROM support_nyzma
+    WHERE
+      project_na LIKE '%25${string.toUpperCase()}%25' OR LOWER(ulurpno) LIKE LOWER('%25${string}%25')
+    LIMIT 5
+  `;
+
+  return carto.SQL(SQL).then(rows =>
+    rows.map((row) => {
+      row.label = toTitleCase(row.project_na);
+      row.type = 'zma';
+      delete row.project_na;
+      return row;
+    }));
+}
+
 router.get('/', (req, res) => {
   const { q } = req.query;
-  const mapzen = mapzenSearch(q);
-  const pluto = plutoSearch(q);
 
-  Promise.all([mapzen, pluto])
+  Promise.all([
+    mapzenSearch(q),
+    plutoSearch(q),
+    zmaSearch(q),
+  ])
     .then((values) => {
-      const [addresses, lots] = values;
+      const [addresses, lots, zmas] = values;
       const responseArray = [];
-      res.json(responseArray.concat(addresses, lots));
+      res.json(responseArray.concat(addresses, lots, zmas));
     }).catch((reason) => {
-      console.log(reason);
+      console.error(reason); // eslint-disable-line
     });
-    // .then((mapzenResults) => {
-    //   const obj = JSON.parse(mapzenResults);
-    //   res.json(obj);
-    // });
 });
 
 module.exports = router;
