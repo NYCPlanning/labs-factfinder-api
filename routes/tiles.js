1faef3b5a15b99259d5b914fb12f81d0fcc39d94
Chris Whong <chris@whong.org>
2017-09-25 00:16:47 -0400

    tile bound definitions working

--- routes/tiles.js
+++ routes/tiles.js
@@ -4,7 +4,7 @@ const SphericalMercator = require('sphericalmercator');
 const proj4 = require('proj4');
 
 const merc = new SphericalMercator({
-  size: 512
+  size: 512,
 });
 
 const bounds = [
@@ -36,42 +36,6 @@ const epsg2263 = '+proj=lcc +lat_1=41.03333333333333 +lat_2=40.66666666666666 +l
 
 const router = express.Router();
 
-router.get('/dtm/:z/:x/:y.png', (req, res) => {
-  const { z, x, y } = req.params;
-
-  const mercbbox = merc.bbox(x, y, z, false); // [w, s, e, n]
-
-
-  const ws = proj4(epsg2263, [mercbbox[0], mercbbox[1]]);
-  const en = proj4(epsg2263, [mercbbox[2], mercbbox[3]]);
-
-  const bbox = [
-    ws[0], ws[1], en[0], en[1],
-  ];
-
-  // console.log(mercbbox, bbox);
-
-
-  const widthInMeters = en[0] - ws[0];
-  const metersPerPixel = widthInMeters / 256;
-
-  // get the first resolution that is higher than that needed by this tile
-  const zoomResolution = resolutions.reduce((acc, cur) => { // eslint-disable-line
-    return (cur > metersPerPixel) ? cur : acc;
-  });
-
-  console.log('next highest res', zoomResolution)
-
-  const tileBounds = getTileBounds(bbox[0], bbox[1], zoomResolution);
-
-  res.send(`X distance: ${widthInMeters}m, ${metersPerPixel} meters/pixel`)
-
-  const tilebbox = `${tileBounds.xmin},${tileBounds.ymin},${tileBounds.xmax},${tileBounds.ymax}`
-  const sourceTile = `http://maps1.nyc.gov/geowebcache/service/wms/?service=WMS&request=GetMap&version=1.1.1&format=image/png&layers=dtm&srs=EPSG:2263&width=512&height=512&bbox=${tilebbox}`;
-
-  console.log(sourceTile);
-
-});
 function getTileBounds(x, y, zoomResolution) {
   const tileBounds = {};
 
@@ -99,5 +63,70 @@ function getTileBounds(x, y, zoomResolution) {
 
   return tileBounds;
 }
+
+function bboxDifferent(a, b) {
+  return (JSON.stringify(a) !== JSON.stringify(b));
+}
+
+function getAllTileBounds(bbox, zoomResolution) {
+  // start with nw
+  // wsen 0123
+  const allTileBounds = {};
+  const nw = getTileBounds(bbox[0], bbox[3], zoomResolution);
+  allTileBounds.nw = nw;
+
+  const ne = getTileBounds(bbox[2], bbox[3], zoomResolution);
+  if (bboxDifferent(ne, nw)) allTileBounds.ne = ne;
+
+  const sw = getTileBounds(bbox[0], bbox[1], zoomResolution);
+  if (bboxDifferent(sw, nw)) allTileBounds.sw = sw;
+
+  const se = getTileBounds(bbox[2], bbox[1], zoomResolution);
+  if (allTileBounds.ne && allTileBounds.sw) allTileBounds.se = se;
+
+  return allTileBounds;
+}
+
+router.get('/dtm/:z/:x/:y.png', (req, res) => {
+  const { z, x, y } = req.params;
+
+  const mercbbox = merc.bbox(x, y, z, false); // [w, s, e, n]
+
+
+  const ws = proj4(epsg2263, [mercbbox[0], mercbbox[1]]);
+  const en = proj4(epsg2263, [mercbbox[2], mercbbox[3]]);
+
+  const bbox = [
+    ws[0], ws[1], en[0], en[1],
+  ];
+
+  // console.log(mercbbox, bbox);
+
+
+  const widthInMeters = en[0] - ws[0];
+  const metersPerPixel = widthInMeters / 256;
+
+  // get the first resolution that is higher than that needed by this tile
+  const zoomResolution = resolutions.reduce((acc, cur) => { // eslint-disable-line
+    return (cur > metersPerPixel) ? cur : acc;
+  });
+
+  console.log('next highest res', zoomResolution);
+
+  const allTileBounds = getAllTileBounds(bbox, zoomResolution);
+
+  console.log(allTileBounds);
+
+  Object.keys(allTileBounds).forEach((key) => {
+    const tileBounds = allTileBounds[key];
+    const tilebbox = `${tileBounds.xmin},${tileBounds.ymin},${tileBounds.xmax},${tileBounds.ymax}`;
+    const sourceTile = `http://maps1.nyc.gov/geowebcache/service/wms/?service=WMS&request=GetMap&version=1.1.1&format=image/png&layers=dtm&srs=EPSG:2263&width=512&height=512&bbox=${tilebbox}`;
+
+    console.log(key, sourceTile);
+  })
+
+  res.send(`X distance: ${widthInMeters}m, ${metersPerPixel} meters/pixel`)
+});
+
 
 module.exports = router;
