e3b9534103616fe1f982856ae82ff8a8078cefc3
Chris Whong <chris@whong.org>
2017-10-05 21:57:46 -0400

    remove tiles endpoint

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const search = require('./search');
-const tiles = require('./tiles');
 
 const router = express.Router();
 
 router.use('/search', search);
-router.use('/tiles', tiles);
 
 module.exports = router;
