a5fcc5cc28740ff317dee360d2bb92a12775ae31
Chris Whong <chris@whong.org>
2017-11-19 08:01:40 -0500

    setup validation and get response

--- models/selection.js
+++ models/selection.js
@@ -1,8 +1,24 @@
 const mongoose = require('mongoose');
 
 const SelectionSchema = new mongoose.Schema({
-  type: String,
-  geoids: Array,
+  type: {
+    type: String,
+    required: true,
+    enum: ['block', 'tract', 'nta', 'puma'],
+  },
+  geoids: {
+    type: [{
+      type: String,
+      minlength: 4,
+      maxlength: 7,
+    }],
+    required: true,
+  },
+  hash: {
+    type: String,
+    length: 6,
+    required: true,
+  },
 });
 
 const Selection = mongoose.model('Selection', SelectionSchema);
--- routes/selection.js
+++ routes/selection.js
@@ -1,26 +1,62 @@
 const express = require('express');
+const sha1 = require('sha1');
+
 const Selection = require('../models/selection');
 
 const router = express.Router();
 
-router.get('/:selectionid', (req, res) => {
-  res.send(`GOT ${req.params.selectionid}`);
+router.get('/:hash', (req, res) => {
+  const { hash } = req.params;
+  Selection.findOne({ hash })
+    .then((match) => {
+      const { type, geoids } = match;
+      res.send({
+        type,
+        geoids,
+      });
+    });
 });
 
-router.post('/', (req, res) => {
-  const { type, geoids } = req.body;
 
+router.post('/', (req, res) => {
+  const { body } = req;
+  const { type, geoids } = body;
+
+  body.geoids.sort();
+
+  const hash = sha1(JSON.stringify(body)).substring(0, 8);
   const selection = new Selection({
     type,
     geoids,
+    hash,
   });
 
-  selection.save((err) => {
-    if (err) throw err;
-
-    console.log('Selection saved successfully!');
-    res.send('Saved!')
-  });
+  // lookup hash in db
+  Selection.findOne({ hash })
+    .then((match) => {
+      if (match) {
+        res.send({
+          status: 'existing selection found',
+          hash,
+        });
+      } else {
+        selection.save()
+          .then(() => {
+            res.send({
+              status: 'new selection saved',
+              hash,
+            });
+          })
+          .catch(((err) => {
+            res.send({
+              status: `error: ${err}`,
+            });
+          }));
+      }
+    });
 });
 
 module.exports = router;
+
+// hash submission => check to see if hash already exists in database =>
+// if it does, return looked up hash, if not return new hash
