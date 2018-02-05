20fbd5a36c1f4749fdf6ceb24015e452e53950a5
Chris Whong <chris@whong.org>
2018-02-05 14:45:02 -0500

    set up profile route

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const search = require('./search');
 const selection = require('./selection');
+const profile = require('./profile');
 
 const router = express.Router();
 
 router.use('/search', search);
 router.use('/selection', selection);
+router.use('/profile', profile);
 
 module.exports = router;
