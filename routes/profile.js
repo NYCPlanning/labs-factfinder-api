821a9644d339cba229e9248ed91e72afa3da3273
Chris Whong <chris@whong.org>
2018-02-05 16:32:38 -0500

    profile route prototype complete

--- routes/profile.js
+++ routes/profile.js
@@ -29,13 +29,20 @@ function buildSQL(profile, ids, comparator = 0) {
 
       main_numbers AS (
         SELECT
-          sum(e),
-          base,
-          category,
-          variable,
-          dataset
-        FROM enriched_selection
-        GROUP BY variable, dataset, base, category
+          *,
+          (((m / 1.645) / NULLIF(SUM,0)) * 100) AS cv
+        FROM (
+          SELECT
+            sum(e),
+            sqrt(sum(power(m, 2))) AS m,
+            base,
+            category,
+            variable,
+            profile,
+            dataset
+          FROM enriched_selection
+          GROUP BY variable, dataset, base, category, profile
+        ) x
       ),
 
       base_numbers AS (
@@ -43,7 +50,7 @@ function buildSQL(profile, ids, comparator = 0) {
           sum(e) AS base_sum,
           sqrt(sum(power(m, 2))) AS base_m,
           max(base) AS base_join,
-          max(dataset) AS base_year
+          max(dataset) AS base_dataset
         FROM enriched_selection
         WHERE base = variable
         GROUP BY variable, dataset
@@ -64,12 +71,18 @@ function buildSQL(profile, ids, comparator = 0) {
 
       comparison_main_numbers AS (
         SELECT
-          sum(e) AS comparison_sum,
-          sqrt(sum(power(m, 2))) AS comparison_m,
-          base AS comparison_join,
-          dataset AS comparison_year
-        FROM comparison_enriched_selection
-        GROUP BY variable, dataset, base
+          *,
+          (((comparison_m / 1.645) / comparison_sum) * 100) AS comparison_cv
+        FROM (
+          SELECT
+            sum(e) AS comparison_sum,
+            sqrt(sum(power(m, 2))) AS comparison_m,
+            base AS comparison_join,
+            variable AS comparison_variable,
+            dataset AS comparison_dataset
+          FROM comparison_enriched_selection
+          GROUP BY variable, dataset, base
+        ) x
       ),
 
       comparison_base_numbers AS (
@@ -77,13 +90,66 @@ function buildSQL(profile, ids, comparator = 0) {
           sum(e) AS comparison_base_sum,
           sqrt(sum(power(m, 2))) AS comparison_base_m,
           base AS comparison_base_join,
-          dataset AS comparison_base_year
+          dataset AS comparison_base_dataset
         FROM comparison_enriched_selection
         WHERE base = variable
         GROUP BY variable, dataset, base
       )
 
-    SELECT * FROM comparison_base_numbers
+
+
+    SELECT
+      base,
+      base_m,
+      base_sum,
+      base_dataset,
+      category,
+      comparison_base_m,
+      comparison_base_sum,
+      comparison_base_dataset,
+      comparison_cv,
+      comparison_m,
+      comparison_sum,
+      cv,
+      dataset,
+      m,
+      sum,
+      variable AS variablename,
+
+      ROUND((comparison_sum / NULLIF(comparison_base_sum,0))::numeric, 4) as comparison_percent,
+
+      CASE
+        WHEN (POWER(comparison_m, 2) %2D POWER(comparison_sum / NULLIF(comparison_base_sum,0), 2) * POWER(comparison_base_m, 2)) < 0
+          THEN (1 / NULLIF(comparison_base_sum,0)) * SQRT(POWER(comparison_m, 2) %2B POWER(comparison_sum / NULLIF(comparison_base_sum,0), 2) * POWER(comparison_base_m, 2))
+        ELSE (1 / NULLIF(comparison_base_sum,0)) * SQRT(POWER(comparison_m, 2) %2D POWER(comparison_sum / NULLIF(comparison_base_sum,0), 2) * POWER(comparison_base_m, 2))
+      END as comparison_percent_m,
+
+      ENCODE(CONVERT_TO(variable || dataset, 'UTF-8'), 'base64') As id,
+
+      ROUND((SUM / NULLIF(base_sum,0))::numeric, 4) as percent,
+
+      CASE
+        WHEN (POWER(m, 2) %2D POWER(sum / NULLIF(base_sum,0), 2) * POWER(base_m, 2)) < 0
+          THEN (1 / NULLIF(base_sum,0)) * SQRT(POWER(m, 2) %2B POWER(sum / NULLIF(base_sum,0), 2) * POWER(base_m, 2))
+        ELSE (1 / NULLIF(base_sum,0)) * SQRT(POWER(m, 2) %2D POWER(sum / NULLIF(base_sum,0), 2) * POWER(base_m, 2))
+      END as percent_m,
+
+      regexp_replace(lower(profile), '[^A-Za-z0-9]', '_', 'g') AS profile,
+
+      regexp_replace(lower(variable), '[^A-Za-z0-9]', '_', 'g') AS variable
+    FROM main_numbers
+
+    INNER JOIN comparison_main_numbers
+      ON main_numbers.variable = comparison_main_numbers.comparison_variable
+      AND main_numbers.dataset = comparison_main_numbers.comparison_dataset
+
+    LEFT OUTER JOIN base_numbers
+      ON main_numbers.base = base_numbers.base_join
+      AND main_numbers.dataset = base_numbers.base_dataset
+
+    LEFT OUTER JOIN comparison_base_numbers
+      ON main_numbers.base = comparison_base_numbers.comparison_base_join
+      AND dataset = comparison_base_numbers.comparison_base_dataset
   `;
 }
 
