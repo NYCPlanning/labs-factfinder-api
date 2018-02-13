0efaf0b5a63a4d3c47e77318f40d5677e805fca3
EricaMaurer <32674674+EricaMaurer@users.noreply.github.com>
2018-02-12 20:33:33 -0500

    Change: median age

--- table-config/demographic/sex-and-age.js
+++ table-config/demographic/sex-and-age.js
@@ -170,31 +170,8 @@ module.exports = [
         column: 'previous_sum',
         aggregator: interpolate,
         options: {
-          bins: [
-            ['mdpop0t4', [0, 4]],
-            ['mdpop5t9', [5, 9]],
-            ['mdpop10t14', [10, 14]],
-            ['mdpop15t17', [15, 17]],
-            ['mdpop18t19', [18, 19]],
-            ['mdpop20', [20, 20]],
-            ['mdpop21', [21, 21]],
-            ['mdpop22t24', [22, 24]],
-            ['mdpop25t29', [25, 29]],
-            ['mdpop30t34', [30, 34]],
-            ['mdpop35t39', [35, 39]],
-            ['mdpop40t44', [40, 44]],
-            ['mdpop45t49', [45, 49]],
-            ['mdpop50t54', [50, 54]],
-            ['mdpop55t59', [55, 59]],
-            ['mdpop60t61', [60, 61]],
-            ['mdpop62t64', [62, 64]],
-            ['mdpop65t66', [65, 66]],
-            ['mdpop67t69', [67, 69]],
-            ['mdpop70t74', [70, 74]],
-            ['mdpop75t79', [75, 79]],
-            ['mdpop80t84', [80, 84]],
-            ['mdpop85pl', [85, 115]],
-          ],
+          referenceSumKey: 'previous_sum',
+          bins: binsForMDAGE,
         },
       },
       {
@@ -240,6 +217,7 @@ module.exports = [
         column: 'comparison_sum',
         aggregator: interpolate,
         options: {
+          referenceSumKey: 'previous_sum',          
           bins: [
             ['mdpop0t4', [0, 4]],
             ['mdpop5t9', [5, 9]],
@@ -271,6 +249,7 @@ module.exports = [
         column: 'comparison_m',
         aggregator: calculateMedianError,
         options: {
+          referenceSumKey: 'previous_m',          
           designFactor: 1.1,
           bins: [
             ['mdpop0t4', [0, 4]],
@@ -307,6 +286,13 @@ module.exports = [
         },
       },
       {
+        column: 'previous_cv',
+        aggregator: formula,
+        options: {
+          formula: '((GET("mdage.previous_m")/ 1.645) / GET("mdage.previous_sum") * 100)',
+        },
+      },
+      {
         column: 'difference_sum',
         aggregator: formula,
         options: {
@@ -325,6 +311,13 @@ module.exports = [
         aggregator: formula,
         options: {
           formula: '(GET("mdage.sum") - GET("mdage.previous_sum"))',
+        },
+      },
+      {
+        column: 'change_m',
+        aggregator: formula,
+        options: {
+          formula: 'SQRT(POWER(GET("mdage.m"),2) + POWER(GET("mdage.previous_m"),2))',
         },
       },
     ],
