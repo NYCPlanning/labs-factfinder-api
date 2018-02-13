955a2b1ba6d86e768e28b2d56c1a5cf00195fb6c
EricaMaurer <32674674+EricaMaurer@users.noreply.github.com>
2018-02-13 12:20:50 -0500

    Change: avg hhsize and avgfamsize
    Added ABS to difference MOE

--- table-config/decennial/relationship-to-head-of-household--householder.js
+++ table-config/decennial/relationship-to-head-of-household--householder.js
@@ -109,9 +109,23 @@ module.exports = [
         column: 'difference_m',
         aggregator: formula,
         options: {
-          formula: 'SQRT(POWER(GET("avghhsz.m"),2) + POWER(GET("avghhsz.comparison_m"),2))',
+          formula: 'ABS(SQRT(POWER(GET("avghhsz.m"),2) + POWER(GET("avghhsz.comparison_m"),2)))',
         },
-      },      
+      },
+      {
+        column: 'change_sum',
+        aggregator: formula,
+        options: {
+          formula: '(GET("avghhsz.sum") - GET("avghhsz.previous_sum"))',
+        },
+      },
+      {
+        column: 'change_m',
+        aggregator: formula,
+        options: {
+          formula: 'ABS(SQRT(POWER(GET("avghhsz.m"),2) + POWER(GET("avghhsz.previous_m"),2)))',
+        },
+      },
     ],
   },
   {
@@ -146,9 +160,23 @@ module.exports = [
         column: 'difference_m',
         aggregator: formula,
         options: {
-          formula: 'SQRT(POWER(GET("avgfamsz.m"),2) + POWER(GET("avgfamsz.comparison_m"),2))',
+          formula: 'ABS(SQRT(POWER(GET("avgfamsz.m"),2) + POWER(GET("avgfamsz.comparison_m"),2)))',
         },
-      },      
+      },
+      {
+        column: 'change_sum',
+        aggregator: formula,
+        options: {
+          formula: '(GET("avgfamsz.sum") - GET("avgfamsz.previous_sum"))',
+        },
+      },
+      {
+        column: 'change_m',
+        aggregator: formula,
+        options: {
+          formula: 'ABS(SQRT(POWER(GET("avgfamsz.m"),2) + POWER(GET("avgfamsz.previous_m"),2)))',
+        },
+      },
     ],
   },
   {
