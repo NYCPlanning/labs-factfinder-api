4e4b2f71704ddf8d92699b3f13a7e7e773b2d1fb
allthesignals <wilburnforce@gmail.com>
2018-03-06 14:22:03 -0500

    Fix package json scripts

--- test/profile.test.js
+++ test/profile.test.js
@@ -1,10 +1,29 @@
-const expect = require('expect');
-const request = require('supertest');
-const http = require('http');
+const expect  = require('chai').expect;
+const request = require('request');
+
+const dotenv = require('dotenv');
+dotenv.load();
+
 const app = require('../app');
 
-const server = http.createServer(app);
+app.listen(8080);
 
-const port = (process.env.NODE_ENV === 'production') ? process.env.PORT : 4000;
+it('get a response', function(done) {
+  request('http://localhost:8080/profile/1/demographic?compare=0' , function(error, response, body) {
+    const [json] = JSON.parse(body);
+    expect(response.statusCode).to.equal(200);
+    done();
+  });
+});
 
-server.listen(port);
+describe('Special variables', function() {
+  it('top-bottom codes variables correctly', function(done) {
+    request('http://localhost:8080/profile/726/economic?compare=0' , function(error, response, body) {
+      const medianFamInc = JSON.parse(body).find(obj => {
+        return obj.variable === 'mdfaminc' && obj.dataset === 'y2012_2016';
+      });
+      expect(Object.keys(medianFamInc.codingThresholds).length).to.equal(200);
+      done();
+    });
+  })
+});
