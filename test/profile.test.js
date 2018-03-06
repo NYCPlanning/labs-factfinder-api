c7cdf9bc6fce4c5fa211ee84b1755f37698e84e8
allthesignals <wilburnforce@gmail.com>
2018-03-06 18:44:35 -0500

    Failing test for missing pct point change calcs

--- test/profile.test.js
+++ test/profile.test.js
@@ -22,8 +22,34 @@ describe('Special variables', function() {
       const medianFamInc = JSON.parse(body).find(obj => {
         return obj.variable === 'mdfaminc' && obj.dataset === 'y2012_2016';
       });
+
+      // it should have two properties in it, "sum" and "previous_sum"
+      // this gets the keys of the thresholds object and counts them
+      // to make sure
       expect(Object.keys(medianFamInc.codingThresholds).length).to.equal(2);
       done();
     });
-  })
+  });
+});
+
+describe('normal variables with complex case logic', function() {
+  it('data with 0-estimate current should get pct change calculations: lgfrlep1 change_percentage_point should be calculated', function(done) {
+    request('http://localhost:8080/profile/733/social?compare=4104' , function(error, response, body) {
+      const medianFamInc = JSON.parse(body).find(obj => {
+        return obj.variable === 'lgfrlep1' && obj.dataset === 'y2012_2016';
+      });
+      expect(!!medianFamInc.change_percentage_point).to.equal(true);
+      done();
+    });
+  });
+
+  it('data with 0-estimate previous should get pct change calculations: lgthalep1 change_percentage_point should be calculated', function(done) {
+    request('http://localhost:8080/profile/733/social?compare=4104' , function(error, response, body) {
+      const medianFamInc = JSON.parse(body).find(obj => {
+        return obj.variable === 'lgthalep1' && obj.dataset === 'y2012_2016';
+      });
+      expect(!!medianFamInc.change_percentage_point).to.equal(true);
+      done();
+    });
+  });
 });
