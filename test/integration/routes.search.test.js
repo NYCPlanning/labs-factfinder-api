ffaa6eb36906f8950d0ca42c168ed4516fe79406
Chris Whong <chris@whong.org>
2018-08-03 14:10:16 -0400

    add integration tests for search api

---
+++ test/integration/routes.search.test.js
@@ -0,0 +1,53 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+// const { expect } = chai;
+chai.use(chaiHttp);
+const should = chai.should();
+
+const dotenv = require('dotenv');
+
+dotenv.load();
+
+const server = require('../../app');
+
+describe('GET /search', () => {
+  it('returns a JSON array with objects', (done) => {
+    chai.request(server)
+      .get('/search?q=new%20dorp')
+      .end((err, res) => {
+        should.not.exist(err);
+        res.status.should.equal(200);
+        res.type.should.equal('application/json');
+
+        const array = res.body;
+        // response should be an array
+        array.length.should.be.above(0);
+        // first element in array should be an object
+        (typeof array[0]).should.equal('object');
+        done();
+      });
+  });
+
+  it('requires q as a query param', (done) => {
+    chai.request(server)
+      .get('/search')
+      .end((err, res) => {
+        const array = res.body;
+        // response should be an array
+        array.length.should.equal(0);
+        done();
+      });
+  });
+
+  it('returns results if q is null', (done) => {
+    chai.request(server)
+      .get('/search?q=')
+      .end((err, res) => {
+        const array = res.body;
+        // response should be an array
+        array.length.should.be.above(0);
+        done();
+      });
+  });
+});
