e2b8bc834d7420ddca9d5111d567d1fb7a726664
godfreyyeung <godfreyyeung@gmail.com>
2021-09-02 17:35:32 -0400

    4492 Set acs row 'profile' values to 'acs'

--- query-helpers/profile.js
+++ query-helpers/profile.js
@@ -139,10 +139,7 @@ const profileSQL = (profile, ids, isPrevious = false) => `
         '[^A-Za-z0-9]', '_', 'g'
       ) AS category,
       --- profile ---
-      REGEXP_REPLACE(
-        LOWER(profile),
-        '[^A-Za-z0-9]', '_', 'g'
-      ) AS profile
+      'acs' AS profile
     FROM enriched_profile
     GROUP BY variable, variablename, base, category, profile
     ORDER BY variable, base, category
