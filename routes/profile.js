b9ccffff2e5ca41e2adac752da64465c27d9d094
godfreyyeung <godfreyyeung@gmail.com>
2021-09-09 14:52:48 -0400

    4843 Handle cities geotype-NYC geotype geoid combo

--- routes/profile.js
+++ routes/profile.js
@@ -12,8 +12,6 @@ const router = express.Router();
 
 function convertBoroughLabelToCode(potentialBoroughLabel) {
   switch (potentialBoroughLabel) {
-    case 'NYC':
-        return '0';
     case 'Manhattan':
         return '1';
     case 'Bronx':
@@ -60,6 +58,23 @@ router.get('/:profile/:geotype/:geoid/', async (req, res) => {
 
   if (geotype === 'boroughs') {
     geoid = convertBoroughLabelToCode(geoid);
+  }
+
+  if (geotype === 'cities') {
+    switch (geoid) {
+      case 'NYC':
+        geoid = '0';
+      case 'New%20York%20City':
+        geoid = '0';
+      case 'New York City':
+        geoid = '0';
+      case '0':
+        break;
+      default:
+        res.status(500).send({
+          status: `error: Invalid ID`,
+        });
+    }
   }
 
   if (invalidCompare(compareTo)) res.status(500).send({ error: 'invalid compareTo param' });
