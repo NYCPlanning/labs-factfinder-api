052f5fac55c3bd087919aa135f5b1f752997d750
sptkl <caobaiyue@gmail.com>
2021-09-13 15:18:11 -0400

    trying to avoid arguments too long

--- migrations/metadata.sh
+++ migrations/metadata.sh
@@ -20,18 +20,20 @@ if [ ! -z "$datasource" ] && [ ! -z "$year_curr" ] && [ ! -z "$year_prev" ]; the
     base_url="https://raw.githubusercontent.com/NYCPlanning/db-factfinder/dev/factfinder/data"
     url_curr="$base_url/$datasource/$year_curr/metadata.json"
     url_prev="$base_url/$datasource/$year_prev/metadata.json"
+    CONTENT_CURR="$(curl -s $url_curr)"
+    CONTENT_PREV="$(curl -s $url_prev)"
     if [ $datasource == "acs" ]; then
         echo "$datasource"
         psql $DATABASE_URL \
-            -v CONTENT_CURR="$(curl -s $url_curr)" \
-            -v CONTENT_PREV="$(curl -s $url_prev)" \
+            -v CONTENT_CURR="$CONTENT_CURR" \
+            -v CONTENT_PREV="$CONTENT_PREV" \
             -v YEAR_CURR="$year_curr" \
             -v YEAR_PREV="$year_prev" \
             -f migrations/acs_metadata.sql
     else
         psql $DATABASE_URL \
-            -v CONTENT_CURR="$(curl -s $url_curr)" \
-            -v CONTENT_PREV="$(curl -s $url_prev)" \
+            -v CONTENT_CURR="$CONTENT_CURR" \
+            -v CONTENT_PREV="$CONTENT_PREV" \
             -v YEAR_CURR="$year_curr" \
             -v YEAR_PREV="$year_prev" \
             -f migrations/decennial_metadata.sql
