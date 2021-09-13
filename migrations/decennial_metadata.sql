270e4b2537d5880bf72f04e22f198e43898eb5f0
sptkl <caobaiyue@gmail.com>
2021-09-13 11:55:21 -0400

    adding decennial migration

--- migrations/decennial_metadata.sql
+++ migrations/decennial_metadata.sql
@@ -2,30 +2,30 @@ CREATE TEMP TABLE meta ( _json text , release_year text );
 INSERT INTO meta VALUES(:'CONTENT_CURR', :'YEAR_CURR');
 INSERT INTO meta VALUES(:'CONTENT_PREV', :'YEAR_PREV');
 
-CREATE SCHEMA IF NOT EXISTS acs;
-DROP TABLE IF EXISTS acs.metadata;
-CREATE TABLE acs.metadata (
+CREATE SCHEMA IF NOT EXISTS decennial;
+DROP TABLE IF EXISTS decennial.metadata;
+CREATE TABLE decennial.metadata (
     variablename text,
     release_year text,
     category text,
-    base text
+    relation text
 );
 
-INSERT INTO acs.metadata (
-    SELECT 
+INSERT INTO decennial.metadata (
+    SELECT
         variablename,
         array_to_string(array_agg(release_year), ', '),
         category,
-        base
+        relation
     FROM (
         SELECT
             record ->> 'pff_variable' as variablename,
             release_year,
             record ->> 'category' as category,
-            record ->> 'base_variable' as base
-        FROM (SELECT json_array_elements(_json::json) AS record, release_year FROM meta) t
-        WHERE record ->> 'domain' IN ('social', 'economic', 'housing', 'demographic')
-    ) a
-    GROUP BY variablename, base, category
-    ORDER BY variablename, base, category
+            record ->> 'base_variable' as relation
+        FROM (SELECT json_array_elements(_json::json) AS record, _year FROM meta) t
+        WHERE record ->> 'domain' IN ('decennial')
+    ) a 
+    GROUP BY variablename, relation, category
+    ORDER BY variablename, relation, category
 );
--- migrations/metadata.sh
+++ migrations/metadata.sh
@@ -20,12 +20,22 @@ if [ ! -z "$datasource" ] && [ ! -z "$year_curr" ] && [ ! -z "$year_prev" ]; the
     base_url="https://raw.githubusercontent.com/NYCPlanning/db-factfinder/dev/factfinder/data"
     url_curr="$base_url/$datasource/$year_curr/metadata.json"
     url_prev="$base_url/$datasource/$year_prev/metadata.json"
-    psql $DATABASE_URL \
-        -v CONTENT_CURR="$(curl -s $url_curr)" \
-        -v CONTENT_PREV="$(curl -s $url_prev)" \
-        -v YEAR_CURR="$year_curr" \
-        -v YEAR_PREV="$year_prev" \
-        -f migrations/acs_metadata.sql
+    if [ $datasource == "acs" ]; then
+        echo "$datasource"
+        psql $DATABASE_URL \
+            -v CONTENT_CURR="$(curl -s $url_curr)" \
+            -v CONTENT_PREV="$(curl -s $url_prev)" \
+            -v YEAR_CURR="$year_curr" \
+            -v YEAR_PREV="$year_prev" \
+            -f migrations/acs_metadata.sql
+    else
+        psql $DATABASE_URL \
+            -v CONTENT_CURR="$(curl -s $url_curr)" \
+            -v CONTENT_PREV="$(curl -s $url_prev)" \
+            -v YEAR_CURR="$year_curr" \
+            -v YEAR_PREV="$year_prev" \
+            -f migrations/decennial_metadata.sql
+    fi
 else
     echo "incomplete information, please specify source, year_curr and year_prev"
 fi
