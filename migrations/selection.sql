e9b8ab32675faa45e981607e9e7c0c7540ed917d
TylerMatteo <tylermatteo434@gmail.com>
2021-10-05 13:46:09 -0400

    Missing paren

--- migrations/selection.sql
+++ migrations/selection.sql
@@ -2,7 +2,7 @@
 This will be used to store custom geography selections
 */
 CREATE TABLE IF NOT EXISTS selection (
-	geotype text CHECK(geotype IN ('blocks','tracts','ntas','cdtas','boroughs','districts'),
+	geotype text CHECK(geotype IN ('blocks','tracts','ntas','cdtas','boroughs','districts')),
 	geoids text[] CHECK(array_length(geoids, 1) BETWEEN 2 AND 12),
 	hash text PRIMARY KEY
 );
