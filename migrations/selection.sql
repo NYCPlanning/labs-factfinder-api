3d32b69530f8d6ede558b3b245a8130e37810a59
TylerMatteo <tylermatteo434@gmail.com>
2021-10-08 15:00:42 -0400

    Removing constraint from selection migration

--- migrations/selection.sql
+++ migrations/selection.sql
@@ -3,6 +3,6 @@ This will be used to store custom geography selections
 */
 CREATE TABLE IF NOT EXISTS selection (
 	geotype text CHECK(geotype IN ('blocks','tracts','ntas','cdtas','boroughs','districts')),
-	geoids text[] CHECK(array_length(geoids, 1) BETWEEN 2 AND 12),
+	geoids text[],
 	hash text PRIMARY KEY
 );
