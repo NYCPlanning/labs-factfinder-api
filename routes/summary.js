cc93d195973df022ddc3e8358aa6a7f781891585
dhochbaum-dcp <dhochbaum@planning.nyc.gov>
2022-07-28 16:44:39 -0400

    Added summary route to be able to show info in the map utility box.

---
+++ routes/summary.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const { DECENNIAL_LATEST_TABLE_FULL_PATH } = require('../special-calculations/data/constants');
+
+const router = express.Router();
+
+/*
+ * Helper function to calculate totals
+ * @param{Object} data - the data to calculate totals with
+ * @param{Object} variableNames - the variable names to calculate totals for
+ */
+function summarizeData(data, variableNames) {
+  var summarizedData = {};
+  variableNames.forEach((variableName) => { summarizedData[variableName] = 0; });
+  data.forEach((item) => { summarizedData[item.variable] += item.value });
+  return summarizedData;
+}
+
+/*
+ * Checks that the summary query parameter is a valid summary type
+ * @param{string} summary - The summary type
+ * @returns{Boolean}
+ */
+function isInvalidSummary(summary) {
+  const validSummaryNames = ['decennial', 'acs'];
+  if (validSummaryNames.includes(summary)) return false;
+  return true;
+}
+
+router.get('/:summary/:summaryvars/:geoids/', async (req, res) => {
+  const { app } = req;
+
+  const { summary, summaryvars, geoids: _geoids } = req.params;
+
+  const varString = "'" + summaryvars.split(',').join("','") + "'";
+  const idString = "'" + _geoids.split(',').join("','") + "'";
+
+  if (isInvalidSummary(summary)) res.status(400).send({ error: 'Invalid summary name. Summary must be acs or decennial' });
+  const summaryData = await app.db.query(`SELECT * FROM ${DECENNIAL_LATEST_TABLE_FULL_PATH} WHERE "geoid" IN (${idString}) and "variable" IN (${varString})`);
+  const totals = summarizeData(summaryData, summaryvars.split(','));
+  return res.send({ totals: totals, summaryData: summaryData });
+});
+
+module.exports = router;
