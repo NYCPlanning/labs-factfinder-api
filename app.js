d18de9f5c5cbe78780df79d1da148fb93800d0a5
Tyler Matteo <tylermatteo434@gmail.com>
2022-11-28 11:49:16 -0500

    import cors package

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 const app = express();
 
