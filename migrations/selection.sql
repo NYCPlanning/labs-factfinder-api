c92047be0fe37807a8e3ac1d1986791043f3d637
tangoyankee <miller.tim108@gmail.com>
2023-08-30 11:51:30 -0400

    populate support_geoids table with ccds update selection table to support ccds

--- migrations/selection.sql
+++ migrations/selection.sql
@@ -2,7 +2,7 @@
 This will be used to store custom geography selections
 */
 CREATE TABLE IF NOT EXISTS selection (
-	geotype text CHECK(geotype IN ('blocks','tracts','ntas','cdtas','boroughs','districts')),
+	geotype text CHECK(geotype IN ('blocks','tracts','ntas','cdtas','boroughs','districts', 'ccds')),
 	geoids text[],
 	hash text PRIMARY KEY
 );
