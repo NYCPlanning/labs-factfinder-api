05ac2d61881ee1000c9a8b56c44070c342076243
tangoyankee <miller.tim108@gmail.com>
2023-09-08 17:10:03 -0400

    add special variable configurations

--- special-calculations/decennial.js
+++ special-calculations/decennial.js
@@ -1,47 +1,50 @@
 module.exports = [
-  // housing-occupancy
-  {
-    variable: 'hmownrvcrt',
-    specialType: 'mean',
-    options: { args: ['vhufslo', 'oochu1'], formulaName: { sum: 'ratio' } },
-  },
-  {
-    variable: 'rntvcrt',
-    specialType: 'mean',
-    options: { args: ['vhufrnt', 'rochu_3'], formulaName: { sum: 'ratio' } },
-  },
-  // housing tenure
-  {
-    variable: 'avhhszooc',
-    specialType: 'mean',
-    options: { args: ['popoochu', 'oochu'] },
-  },
-  {
-    variable: 'avhhszroc',
-    specialType: 'mean',
-    options: { args: ['poprochu', 'rochu_1'] },
-  },
-  // population density
-  {
-    variable: 'popacre',
-    specialType: 'mean',
-    options: { args: ['pop1', 'landacres'] },
-  },
-  // relationship to head of household (householder)
-  {
-    variable: 'avghhsz',
-    specialType: 'mean',
-    options: { args: ['popinhh_1', 'ochu_1'] },
-  },
-  {
-    variable: 'avgfamsz',
-    specialType: 'mean',
-    options: { args: ['popinfam', 'fam1'] },
-  },
-  // sex and age
-  {
-    variable: 'mdage',
-    specialType: 'median',
-    options: {},
-  },
+    //   Population per acre
+    {
+        variable: 'popacre',
+        specialType: 'mean',
+        options: { args: ['pop5', 'landacres'], formulaName: { sum: 'ratio' } },
+    },
+    // Median Age
+    {
+        variable: 'mdage',
+        specialType: 'median',
+        options: {},
+    },
+    // Age dependency ratio
+    {
+        variable: 'agdpdrt',
+        specialType: 'mean',
+        options: { args: ['agu1865p', 'ag18t64'], formulaName: { sum: 'ratio' } }
+    },
+    // Old age dependency ratio
+    {
+        variable: 'odagdpdrt',
+        specialType: 'mean',
+        options: { args: ['odag65pl', 'odag18t64'], formulaName: { sum: 'ratio' } }
+    },
+    // Child dependency ratio
+    {
+        variable: 'chlddpdrt',
+        specialType: 'mean',
+        options: { args: ['cdpdu18', 'cdpd18t64'], formulaName: { sum: 'ratio' } }
+    },
+    // Home owner vacancy rate
+    {
+        variable: 'hmownvcrt',
+        specialType: 'mean',
+        options: { args: ['vacsale', 'hovacu'], formulaName: { sum: 'ratio' } },
+    },
+    // Rental vacancy rate
+    {
+        variable: 'rntvcrt',
+        specialType: 'mean',
+        options: { args: ['vacrnt', 'rntvacu'], formulaName: { sum: 'ratio' } },
+    },
+    // Average household size
+    {
+        variable: 'avghhsz',
+        specialType: 'mean',
+        options: { args: ['popinhh_1', 'ochu_1'] },
+    }
 ];
