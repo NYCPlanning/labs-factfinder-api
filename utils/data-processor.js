157cc5d267ae14572920afe811e9b8e74440b265
horatio <horatiorosa@aol.com>
2024-05-10 11:17:40 -0400

    issue 303 get coded rows to set isReliable to false when top/bottom coded

--- utils/data-processor.js
+++ utils/data-processor.js
@@ -174,7 +174,7 @@ class DataProcessor {
    */
   recalculateIsReliable(row, wasCoded) {
     // top- or bottom-coded values are not reliable
-    if (!row.correlationCoefficient) return;
+    if (!row.correlationCoefficient && !wasCoded) return;
     row.isReliable = wasCoded ? false : executeFormula(this.data, row.variable, 'isReliable');
   }
 }
