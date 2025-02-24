81f1037d9b7568ea41b96fa2f88257f20305353c
pratishta <ypradhi9@gmail.com>
2025-02-24 15:26:11 -0500

    Update inflation and current year

--- special-calculations/data/constants.js
+++ special-calculations/data/constants.js
@@ -1,9 +1,9 @@
 // constants for use in special calculations
-const INFLATION_FACTOR = 1.3493;
+const INFLATION_FACTOR = 1.4049;
 const CORRELATION_COEFFICIENT_CONST = 1.645;
 const DIFF_PERCENT_THRESHOLD = -0.05;
 const DESIGN_FACTOR = 1.5;
-const CUR_YEAR = 'Y2018-2022';
+const CUR_YEAR = 'Y2019-2023';
 const PREV_YEAR = 'Y2006-2010'; // TODO: update with actual previous year
 const GEOGRAPHY_VERSION = '2023-08-29';
 const DECENNIAL_SCHEMA_NAME = 'decennial';
