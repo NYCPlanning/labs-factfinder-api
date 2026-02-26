3feefeebc93963c03634e259f6d4e41a29ba7f0b
David Hochbaum <dhochbaum@planning.nyc.gov>
2026-02-26 15:06:55 -0500

    Added the Azure PFF domains to the CORS policy for the internal sites

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.db = pgp(process.env.DATABASE_URL);
 
 // allows CORS
 app.use(cors({
-  origin: ['https://staging--labs-factfinder.netlify.app', 'https://develop--labs-factfinder.netlify.app', 'https://popfactfinder.planning.nyc.gov', 'http://localhost:4200', 'https://factfinder-staging.planninglabs.nyc', 'https://factfinder-develop.planninglabs.nyc'],
+  origin: ['https://staging--labs-factfinder.netlify.app', 'https://develop--labs-factfinder.netlify.app', 'https://popfactfinder.planning.nyc.gov', 'http://localhost:4200', 'https://factfinder-staging.planninglabs.nyc', 'https://factfinder-develop.planninglabs.nyc', 'https://factfinder.dcp.nycnet', 'https://factfinder-nonprod.dcp.nycnet'],
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   allowedHeaders: 'X-Requested-With,Content-Type,Authorization',
 }));
